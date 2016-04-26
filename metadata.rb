5fcbda73a0954f50076f6405547ae899783c2836
Joshua C. Burt <joshburt@shapeandshare.com>
2016-04-26 11:11:41 -0600

    updated the default location of the git bundled openssl and certs for current releases of git

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Apache Tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.19.0'
+version          '0.20.0'
 
 depends 'java'
 depends 'openssl'
